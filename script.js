6a3c681415c1b78d8abce0b2de321f810f057080
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-09-23 08:05:23 -0300

    Atualizações JavaScript.

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 var numeroSorteado = parseInt(Math.random()*11);
 var elementoResultado = document.getElementById("resultado");
 var tentativas = 3
+
 function Chutar(){
     var chute = parseInt(document.getElementById("valor").value);
     if (tentativas > 0){
         tentativas = tentativas - 1;
     
-
         if(chute == numeroSorteado){
-        elementoResultado.innerHTML = "Acertou";
+        elementoResultado.innerHTML = "Acertou, O número sorteado é: " + numeroSorteado;
 
         }else if (chute > 10 || chute < 0){
         elementoResultado.innerHTML = "Digita números entre 0 e 10";
 
         }else {
         elementoResultado.innerHTML =  "Errou";
+
         }
+        document.getElementById("valor").value = ""
+    
     }else {
         elementoResultado.innerHTML = "Suas tentativas acabaram!"
     }
+
     }
 
 
