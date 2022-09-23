c86c9b231b8da9af1adcb3b6edf6b4faacf6804e
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-09-23 07:48:44 -0300

    Adiciodado funçoes.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 var numeroSorteado = parseInt(Math.random()*11);
 var elementoResultado = document.getElementById("resultado");
+var tentativas = 3
 function Chutar(){
     var chute = parseInt(document.getElementById("valor").value);
-    console.log(chute);
-    if(chute == numeroSorteado){
+    if (tentativas > 0){
+        tentativas = tentativas - 1;
+    
+
+        if(chute == numeroSorteado){
         elementoResultado.innerHTML = "Acertou";
 
-    }else if (chute > 10 || chute < 0){
+        }else if (chute > 10 || chute < 0){
         elementoResultado.innerHTML = "Digita números entre 0 e 10";
 
-    }else {
+        }else {
         elementoResultado.innerHTML =  "Errou";
+        }
+    }else {
+        elementoResultado.innerHTML = "Suas tentativas acabaram!"
     }
-
-}
+    }
 
 
